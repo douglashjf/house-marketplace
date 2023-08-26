e2b0f793177146796789f093dff10152eddf87ff
sean wong <sean.wong.2009@business.smu.edu.sg>
2023-08-26 12:08:34 +0800

    created model-offers and migration updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_26_031839) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_26_040510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_26_031839) do
     t.index ["user_id"], name: "index_houses_on_user_id"
   end
 
+  create_table "offers", force: :cascade do |t|
+    t.integer "price"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "house_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["house_id"], name: "index_offers_on_house_id"
+    t.index ["user_id"], name: "index_offers_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,4 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_26_031839) do
   end
 
   add_foreign_key "houses", "users"
+  add_foreign_key "offers", "houses"
+  add_foreign_key "offers", "users"
 end
