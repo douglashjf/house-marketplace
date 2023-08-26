4d19fe5fd464e6ef77b97475d70581d63ff8475f
Tay Yi Yang <tayyiyang381@gmail.com>
2023-08-26 13:43:38 +0800

    added validations on model house

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,3 +1,11 @@
 class House < ApplicationRecord
+  TENURE_CAT = ["99-year", "999-year", "freehold"]
+  BEDROOM = [1, 2, 3, 4, 5]
+  BATHROOM = [1, 2, 3, 4, 5]
+  PROPERTY_TYPE = ["HDB", "Condo", "Landed"]
+
   belongs_to :user
+  validates :address, :price, :bedroom, :bathroom, :square_feet, :description, :property_type, :tenure, presence: true
+  validates :bedroom, :bathroom, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+
 end
