61d9534dbbbc991503077fbc02bcf1d330f91a3d
Jacob Khong <jacobkhongdev@gmail.com>
2023-08-29 13:29:54 +0800

    update homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @houses = House.all
+    @top_houses = House.order(price: :desc).limit(4)
   end
 end
