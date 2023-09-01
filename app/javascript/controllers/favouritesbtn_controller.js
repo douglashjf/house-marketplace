4be1feda5103c5fb9c1b71def0f9cb78b229f9fb
sean wong <sean.wong.2009@business.smu.edu.sg>
2023-09-01 12:00:50 +0800

    pop up message added for favourites

--- app/javascript/controllers/favouritesbtn_controller.js
+++ app/javascript/controllers/favouritesbtn_controller.js
@@ -2,13 +2,33 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="favouritesbtn"
 export default class extends Controller {
-  static targets = ['favourites'];
+  static targets = ['favourites', 'flashMessageContainer'];
 
   connect() {
   }
 
   toggle() {
     this.favouritesTarget.classList.toggle('is-favourites');
-    this.favouritesTarget.innerText === 'Add to Favourites' ? this.favouritesTarget.innerText = 'Remove from Favourites' : this.favouritesTarget.innerText = 'Add to Favourites';
+    const flashAddMessage = document.createElement('div');
+    flashAddMessage.className = 'flash-add-message';
+    flashAddMessage.textContent = 'Page added to favourites';
+    const flashRemMessage = document.createElement('div');
+    flashRemMessage.className = 'flash-rem-message';
+    flashRemMessage.textContent = 'Page removed from favourites';
+    if (this.favouritesTarget.innerText === '➕ Add to Favourites') {
+      this.favouritesTarget.innerText = '➖ Remove from Favourites';
+      this.flashMessageContainerTarget.appendChild(flashAddMessage);
+      setTimeout(() => {
+        this.flashMessageContainerTarget.removeChild(flashAddMessage);
+      }, 1500);
+    } else {
+      this.favouritesTarget.innerText = '➕ Add to Favourites';
+      this.flashMessageContainerTarget.appendChild(flashRemMessage);
+      setTimeout(() => {
+        this.flashMessageContainerTarget.removeChild(flashRemMessage);
+      }, 1500);
+    }
+
+
   }
 }
