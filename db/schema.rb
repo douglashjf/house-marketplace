f5b9e622a811ea39770040435943b276876d015b
sean wong <sean.wong.2009@business.smu.edu.sg>
2023-09-05 17:26:14 +0800

    pop up message favourites added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_02_055356) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_05_034836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,7 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_02_055356) do
 
   create_table "offers", force: :cascade do |t|
     t.integer "price"
-    t.string "status", default: "pending"
+    t.string "status", default: "Pending"
     t.bigint "user_id", null: false
     t.bigint "house_id", null: false
     t.datetime "created_at", null: false
